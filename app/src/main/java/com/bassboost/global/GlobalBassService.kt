27470b67bfc26e3f02c298441d3449845312c31f
refinadorlegendary-rgb <refinadorlegendary@gmail.com>
2026-07-30 17:10:05 -0500

    Adjust MbcBand parameters in GlobalBassService

--- app/src/main/java/com/bassboost/global/GlobalBassService.kt
+++ app/src/main/java/com/bassboost/global/GlobalBassService.kt
@@ -179,7 +179,7 @@ class GlobalBassService : Service() {
             dp.setPreEqBandAllChannelsTo(3, DynamicsProcessing.EqBand(true, 14000f, tTwister * 12.0f))
 
             val mbcBand = DynamicsProcessing.MbcBand(
-                true, 130f, 0.001f, 0.3f, 16.0f, -18.0f, 16.0f, 0f, 1.0f, 14.0f, 14.0f
+                true, 130f, 0.001f, 0.3f, 16.0f, -18.0f, 16.0f, 0f, 1.0f, 1.0f, 4.0f
             )
             dp.setMbcBandAllChannelsTo(0, mbcBand)
         }
